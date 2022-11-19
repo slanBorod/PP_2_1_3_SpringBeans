10fc5d719c68be0533ff7754abb21c787d48df46
Ruslan <111701991+slanBorod@users.noreply.github.com>
2022-11-19 11:33:54 +0300

    Выполнено задание, тест пройден

--- src/main/java/app/model/Animal.java
+++ src/main/java/app/model/Animal.java
@@ -1,4 +1,10 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public abstract class Animal {
+//    @Autowired
+//    @Qualifier("dog")
+//    private Dog dog;
 }
